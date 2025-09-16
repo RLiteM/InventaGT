4374e8a929efb979364e51055e65b4ccabd77e22
RLiteM <mvicentem6@miumg.edu.gt>
2025-09-16 11:40:49 -0600

    Creacion de Proyto Inventa

---
+++ src/main/java/com/inventa/inventa/InventaApplication.java
@@ -0,0 +1,13 @@
+package com.inventa.inventa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventaApplication.class, args);
+	}
+
+}
