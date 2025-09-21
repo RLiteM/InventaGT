5a9306e6f8c34e2d49179a4605b7f2eb72765aa0
RLiteM <167242629+RLiteM@users.noreply.github.com>
2025-09-21 15:15:49 -0600

    updates

--- src/main/java/com/inventa/inventa/repositories/CategoriaRepository.java
+++ src/main/java/com/inventa/inventa/repositories/CategoriaRepository.java
@@ -1,5 +1,10 @@
 package com.inventa.inventa.repositories;
 
-public class CategoriaRepository {
-    
+import com.inventa.inventa.entities.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+    Categoria findByNombre(String nombre);
 }
--- src/main/java/com/inventa/inventa/repositories/ProductoRepository.java
+++ src/main/java/com/inventa/inventa/repositories/ProductoRepository.java
@@ -1,5 +1,10 @@
 package com.inventa.inventa.repositories;
 
-public class ProductoRepository {
-    
+import com.inventa.inventa.entities.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+    Producto findBySku(String sku);
 }
