bbb08ba565ea828aee03e01e0b04109a7feca687
RLiteM <167242629+RLiteM@users.noreply.github.com>
2025-09-22 11:44:08 -0600

    corregicioens finales

--- src/main/java/com/inventa/inventa/InventaApplication.java
+++ src/main/java/com/inventa/inventa/InventaApplication.java
@@ -5,15 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InventaApplication {
-
     public static void main(String[] args) {
-        // Capturar las variables de entorno que debería inyectar Railway
-        System.out.println("==== VARIABLES DE ENTORNO DETECTADAS ====");
-        System.out.println("SPRING_DATASOURCE_URL=" + System.getenv("SPRING_DATASOURCE_URL"));
-        System.out.println("SPRING_DATASOURCE_USERNAME=" + System.getenv("SPRING_DATASOURCE_USERNAME"));
-        System.out.println("SPRING_DATASOURCE_PASSWORD=" + System.getenv("SPRING_DATASOURCE_PASSWORD"));
-        System.out.println("========================================");
-
         SpringApplication.run(InventaApplication.class, args);
     }
 }
